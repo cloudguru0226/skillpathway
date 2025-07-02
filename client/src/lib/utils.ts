cc434c395cf6ed26d97f2a3e6becc3ea28a58879
Miroslav.Todorov <corporate_it_inf@cbi.ca>
2025-07-02 19:38:08 +0300

    coockies fix 12

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,10 +1,6 @@
-import { scrypt } from 'crypto';
-import { promisify } from 'util';
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-const scryptAsync = promisify(scrypt);
-
-export async function comparePasswords(plainText: string, hashed: string): Promise<boolean> {
-  const [hash, salt] = hashed.split('.');
-  const buf = (await scryptAsync(plainText, salt, 64)) as Buffer;
-  return buf.toString('hex') === hash;
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
