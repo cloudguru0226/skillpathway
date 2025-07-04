6f235282c8e3f182d9a7ee7610715ad2615314c1
Miroslav.Todorov <corporate_it_inf@cbi.ca>
2025-07-04 17:41:33 +0300

    fixing coockies

--- server/auth.ts
+++ server/auth.ts
@@ -37,7 +37,7 @@ export function setupAuth(app: Express) {
     store: storage.sessionStore,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.SESSION_COOKIE_SECURE === "true",
     }
   };
 
